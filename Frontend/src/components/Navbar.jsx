3218921d9d5701464cdcd3c77cbe5cb1695f81ab
octaveweb <karansproject955@gmail.com>
2026-04-29 22:47:00 +0530

    Setup - Base

---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="left">
+        <div className="profile">
+
+
+          <img src="" alt="" />
+
+          <div className="credincles">
+            <div id="name">Abcd_123</div>
+            <div id="user_name">abcd_123</div>
+          </div>
+        </div>
+
+      </div>
+      <div className="right">
+        <p>Call</p>
+        <p>Video Call</p>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
